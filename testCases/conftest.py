3188b8b71fe85399255f57f36b13d785134e60f4
vishalharane1 <vishalbhagwatharane@gmail.com>
2026-07-24 15:57:01 +0530

    driver updated

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -3,7 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+service_edge=Service(EdgeChromiumDriverManager().install())
+service_firefox = Service(
+    GeckoDriverManager().install()
+)
 service = Service(ChromeDriverManager().install())
 chrome_option=Options()
 chrome_option.add_argument("--headless")
@@ -16,13 +22,13 @@ def browser_setup(request):
     browser=request.config.getoption("--browser")
     if browser=="chrome":
         print("\n----> Lunching chrome browser")
-        driver=webdriver.Chrome()
+        driver=webdriver.Chrome(service=service)
     elif browser=="edge":
         print("\n----> Lunching Edge browser")
-        driver=webdriver.Edge()
+        driver=webdriver.Edge(service=service_edge)
     elif browser=="firefox":
         print("\n----> Lunching Firefox browser")
-        driver=webdriver.Firefox()
+        driver=webdriver.Firefox(service=service_firefox)
     elif browser=="headless":
         print("\n----> Lunching headless chrome browser")
         driver=webdriver.Chrome(options=chrome_option)
